974201e6257d457c87e3ff630cbfb3362ecfca78
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2019-02-09 16:22:49 +0300

    Finished the first Barantcev example

--- geometry.py
+++ geometry.py
@@ -7,12 +7,18 @@ class Point:
         self.x = _x
         self.y = _y
 
+    def distance(self, p2):
+        """Calculates the distance between the two points"""
+        dx = self.x - p2.x
+        dy = self.y - p2.y
+        return sqrt(pow(dx, 2) + pow(dy, 2))
 
-def distance(p1, p2):
-    """Calculates the distance between the two dots"""
-    dx = p1.x - p2.x
-    dy = p1.y - p2.y
-    return sqrt(pow(dx, 2) + pow(dy, 2))
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
 
 
-print(distance(Point(0, 0), Point(1, 1)))
+a = Point(0, 0)
+b = Point(3, 8)
+print(a.distance(b))
+print(a == b)
+print(a == Point(0, 0))
