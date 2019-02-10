6f77d1560fd455ef332214b498c7a30a494688ba
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2019-02-10 23:05:48 +0300

    Moved parameters a bit

--- test_add_group.py
+++ test_add_group.py
@@ -9,15 +9,25 @@ class UntitledTestCase(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.OpenHomePage(wd)
+        self.Login(wd, "admin", "secret")
+        self.CreateGroup(wd, "", "", "")
+        self.Logout(wd)
+
     def test_add_group(self):
         wd = self.wd
         self.OpenHomePage(wd)
-        self.Login(wd)
-        self.GoToGroupsPage(wd)
-        self.CreateNewGroup(wd)
-        self.FillGroupData(wd)
-        self.SubmitGroupData(wd)
+        self.Login(wd, "admin", "secret")
+        self.CreateGroup(wd, "Some text 01", "Some text 02", "some text 03")
         self.Logout(wd)
+
+    def CreateGroup(self, wd, group_name, group_header, group_footer):
+        self.GoToGroupPage(wd)
+        self.GoToGroupCreationPage(wd)
+        self.FillGroupData(wd, group_name, group_header, group_footer)
+        self.SubmitGroupData(wd)
 
     def Logout(self, wd):
         # Logout
@@ -27,30 +37,30 @@ class UntitledTestCase(unittest.TestCase):
         # Submit group data
         wd.find_element_by_name("submit").click()
 
-    def FillGroupData(self, wd):
+    def FillGroupData(self, wd, group_name, group_header, group_footer):
         # Fill group data
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("Some text 01")
+        wd.find_element_by_name("group_name").send_keys(group_name)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("Some text 02")
+        wd.find_element_by_name("group_header").send_keys(group_header)
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("some text 03")
+        wd.find_element_by_name("group_footer").send_keys(group_footer)
 
-    def CreateNewGroup(self, wd):
+    def GoToGroupCreationPage(self, wd):
         # Create a new group
         wd.find_element_by_name("new").click()
 
-    def GoToGroupsPage(self, wd):
+    def GoToGroupPage(self, wd):
         # Go to groups page
         wd.find_element_by_link_text("groups").click()
 
-    def Login(self, wd):
+    def Login(self, wd, username, password):
         # Login
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_css_selector("input[value='Login']").click()
 
     def OpenHomePage(self, wd):
