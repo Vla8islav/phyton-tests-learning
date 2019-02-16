e97329dc8d5753fe8531ca2999e36a332d68ddb5
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2019-02-16 11:53:46 +0300

    Added contact modification

--- fixture/contact.py
+++ fixture/contact.py
@@ -6,7 +6,13 @@ class ContactHelper:
     def create(self, c):
         self.open_creation_page()
         self.fill_info_fields(c)
-        self.submit_form()
+        self.submit_new_contact()
+
+    def modify_first_contact(self, c):
+        self.open_list_page()
+        self.modify_first_contact_in_a_list()
+        self.fill_info_fields(c)
+        self.update_contact()
 
     def delete_first_contact(self):
         self.open_list_page()
@@ -14,9 +20,14 @@ class ContactHelper:
         self.click_delete_button()
         self.confirm_contact_deletion()
 
-    def submit_form(self):
+    def submit_new_contact(self):
         self.app.wd.find_element_by_css_selector(
             "input[value='Enter']").click()
+        self.app.wd.find_element_by_link_text("home page").click()
+
+    def update_contact(self):
+        self.app.wd.find_element_by_css_selector(
+            "input[value='Update']").click()
         self.app.wd.find_element_by_link_text("home page").click()
 
     def fill_info_fields(self, contact):
@@ -39,10 +50,10 @@ class ContactHelper:
         self.app.wd.find_element_by_xpath("//a[.='home']").click()
 
     def check_first_contact_in_a_list(self):
-        self.get_all_contacts_in_a_list().click()
+        self.app.wd.find_element_by_css_selector("input[type=checkbox]")
 
-    def get_all_contacts_in_a_list(self):
-        return self.app.wd.find_element_by_css_selector("input[type=checkbox]")
+    def modify_first_contact_in_a_list(self):
+        self.app.wd.find_element_by_css_selector("td img[title='Edit']").click()
 
     def click_delete_button(self):
         self.app.wd.find_element_by_css_selector("input[value='Delete']").click()
