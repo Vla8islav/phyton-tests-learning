61d53b3f78f4dc7e9c68554aacdfdb3133a80a2d
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2019-02-23 16:00:38 +0300

    Renamed the method

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,7 @@ from model.group import Group
 from model.user import User
 
 
-def test_group_create_empty_values(app):
+def test_group_modify_first_group(app):
     group = Group("New value1", "val2", "val3")
     app.session.login(User("admin", "secret"))
     app.group.modify_group(group)
