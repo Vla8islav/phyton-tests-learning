7ec13b0869f237d4f18011edb4ff49dc267565fa
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2019-02-24 13:33:54 +0300

    Fixed bugs in the test

--- test/test_delete_contact.py
+++ test/test_delete_contact.py
@@ -3,5 +3,5 @@ from model.contact import Contact
 
 def test_delete_first_contact(app):
     if app.contact.count() < 1:
-        app.group.create(Contact("New group name"))
+        app.contact.create(Contact("New group name"))
     app.contact.delete_first_contact()
