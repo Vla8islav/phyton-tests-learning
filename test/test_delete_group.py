1c6155582b430a2f488de596fee9f2039e650ed1
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2019-03-03 13:19:15 +0300

    Added simple check for the deletion

--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -4,4 +4,7 @@ from model.group import Group
 def test_delete_first_group(app):
     if app.group.count() < 1:
         app.group.create(Group("New group name"))
+    group_list_at_start = app.group.get_list()
     app.group.delete()
+    group_list_after_creation = app.group.get_list()
+    assert len(group_list_at_start) - 1 == len(group_list_after_creation)
