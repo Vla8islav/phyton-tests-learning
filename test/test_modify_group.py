f46ec8f8e883a9e69ee62d2c2aaacbae41f58520
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2019-03-03 14:38:33 +0300

    Added the group modification compare

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -8,10 +8,10 @@ def test_group_modify_first_group(app):
     group = Group("New value1", "val2", "val3")
     app.group.modify_group(group)
     group_list_at_start = app.group.get_list()
-    group.id = group_list_at_start[0].id
     app.group.modify_group(group)
     group_list_after_modification = app.group.get_list()
     assert len(group_list_at_start) == len(group_list_after_modification)
+    group.id = group_list_at_start[0].id
     expected_new_group_list = group_list_at_start.copy()
     expected_new_group_list[0:1] = [group]
     assert sorted(expected_new_group_list, key=Group.id_with_none) == sorted(group_list_after_modification, key=Group.id_with_none)
@@ -25,3 +25,7 @@ def test_group_modify_first_group_name_only(app):
     app.group.modify_group(group)
     group_list_after_modification = app.group.get_list()
     assert len(group_list_at_start) == len(group_list_after_modification)
+    group.id = group_list_at_start[0].id
+    expected_new_group_list = group_list_at_start.copy()
+    expected_new_group_list[0:1] = [group]
+    assert sorted(expected_new_group_list, key=Group.id_with_none) == sorted(group_list_after_modification, key=Group.id_with_none)
