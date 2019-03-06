2ecdf1ff15bc12f6ab2b667c012fb16b7861fe6f
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2019-03-06 09:06:26 +0300

    Added cashing for contacts

--- fixture/contact.py
+++ fixture/contact.py
@@ -26,11 +26,13 @@ class ContactHelper:
     def submit_new_contact(self):
         self.app.wd.find_element_by_css_selector(
             "input[value='Enter']").click()
+        self.list_cache = None
         self.app.wd.find_element_by_link_text("home page").click()
 
     def update_contact(self):
         self.app.wd.find_element_by_css_selector(
             "input[value='Update']").click()
+        self.list_cache = None
         self.app.wd.find_element_by_link_text("home page").click()
 
     def fill_info_fields(self, contact):
@@ -48,7 +50,7 @@ class ContactHelper:
 
     def open_list_page(self):
         if not ((self.app.wd.current_url.endswith("/") and
-                len(self.app.wd.find_elements_by_css_selector("input[type=button][value='Send e-Mail']")) > 0))\
+                 len(self.app.wd.find_elements_by_css_selector("input[type=button][value='Send e-Mail']")) > 0)) \
                 or not self.app.wd.current_url.endswith("/"):
             self.app.wd.find_element_by_xpath("//a[.='home']").click()
 
@@ -64,27 +66,32 @@ class ContactHelper:
     def confirm_contact_deletion(self):
         alert = self.app.wd.switch_to_alert()
         alert.accept()
+        self.list_cache = None
 
     def count(self):
         self.open_list_page()
         return len(self.app.wd.find_elements_by_css_selector("input[type='checkbox'][name='selected[]']"))
 
+    list_cache = None
+
     def get_list(self):
-        self.open_list_page()
-        self.app.wd.refresh()
-        contact_web_elements = self.app.wd.find_elements_by_css_selector("table tbody tr[name=entry]")
+        if self.list_cache is None:
+            self.open_list_page()
+            self.app.wd.refresh()
+            contact_web_elements = self.app.wd.find_elements_by_css_selector("table tbody tr[name=entry]")
 
-        contact_list = []
-        if contact_web_elements is not None:
-            for contact_row in contact_web_elements:
-                contact_columns = contact_row.find_elements_by_css_selector("td")
-                contact_id = contact_columns[0].find_element_by_css_selector("input").get_property("value")
-                last_name = contact_columns[1].text
-                first_name = contact_columns[2].text
-                address = contact_columns[3].text
-                email = contact_columns[4].text
-                phone_list = contact_columns[5].text
+            self.list_cache = []
+            if contact_web_elements is not None:
+                for contact_row in contact_web_elements:
+                    contact_columns = contact_row.find_elements_by_css_selector("td")
+                    contact_id = contact_columns[0].find_element_by_css_selector("input").get_property("value")
+                    last_name = contact_columns[1].text
+                    first_name = contact_columns[2].text
+                    address = contact_columns[3].text
+                    email = contact_columns[4].text
+                    phone_list = contact_columns[5].text
 
-                contact_list.append(Contact(contact_id=int(contact_id), name=first_name, last_name=last_name, email=email))
+                    self.list_cache.append(
+                        Contact(contact_id=int(contact_id), name=first_name, last_name=last_name, email=email))
 
-        return contact_list
+        return self.list_cache.copy()
