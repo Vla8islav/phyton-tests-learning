4e7aebcb6a9d3d5df3652c5e4ca878abdbf9a11c
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2019-03-31 14:39:07 +0300

    Added the group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,19 +1,8 @@
-import random
-import string
-
-import pytest
-
-from generator.random_string import random_string
 from model.group import Group
 
 
-test_data = [
-    Group(name=random_string("name", 10), header=random_string("header", 10), footer=random_string("footer", 10))
-    for i in range(5)]
-
-
-@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
-def test_group_create_empty_values(app, group):
+def test_group_create(app, json_groups):
+    group = json_groups
     group_list_at_start = app.group.get_list()
     app.group.create(group)
     group_list_after_creation = app.group.get_list()
